dd76415d6f922958a93c42e11b8c4252f0e46ab3
shono <>
2021-07-27 18:04:21 -0700

    Release 0.1.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 
 setuptools.setup(name='cvsutils',
-                 version='0.1.6',
+                 version='0.1.7',
                  description="Unofficial utility scripts for Microsoft Custom Vision Service",
                  packages=setuptools.find_packages(),
                  license='MIT',
